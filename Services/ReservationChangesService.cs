6a9fea1bc1bd30fa5d9d3174738e641aaa14589b
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-06-08 18:21:38 +0200

    reservation change notifications

--- Services/ReservationChangesService.cs
+++ Services/ReservationChangesService.cs
@@ -1,5 +1,7 @@
 ﻿using Reservation.Database;
 using Reservation.Database.Tables;
+using Models.Reservations;
+using MassTransit;
 
 
 namespace Reservation.Services
@@ -7,9 +9,11 @@ namespace Reservation.Services
     public class ReservationChangesService : IReservationChangesService
     {
         private readonly ReservationsContext _context;
-        public ReservationChangesService(ReservationsContext context)
+        private readonly IPublishEndpoint _publishEndpoint;
+        public ReservationChangesService(ReservationsContext context, IPublishEndpoint publishEndpoint)
         {
             _context = context;
+            _publishEndpoint = publishEndpoint;
         }
         public void AddChanges(ReservationChangeEntity reservationChange)
         {
@@ -44,7 +48,25 @@ namespace Reservation.Services
                             ChangeDate = reservationChange.ChangeDate,
                             ReservationAvailable = reservationChange.ReservationAvailable
                         };
+                        var singleReservationChangeEvent = new ChangedReservationEvent()
+                        {
+                            ReservationId = affectedReservation.ReservationId,
+                            HotelId = reservationChange.HotelId,
+                            HotelName = reservationChange.HotelName,
+                            BigRoomNumberChange = reservationChange.BigRoomNumberChange,
+                            SmallRoomNumberChange = reservationChange.SmallRoomNumberChange,
+                            BreakfastAvailable = reservationChange.BreakfastAvailable,
+                            WifiAvailable = reservationChange.WifiAvailable,
+                            ChangeInHotelPrice = reservationChange.ChangeInHotelPrice,
+                            HotelAvailable = reservationChange.HotelAvailable,
+                            TransportId = reservationChange.TransportId,
+                            ChangeInTransportPrice = reservationChange.ChangeInTransportPrice,
+                            FreeSeatsChange = reservationChange.FreeSeatsChange,
+                            PlaneAvailable = reservationChange.PlaneAvailable,
+                            ReservationAvailable = reservationChange.ReservationAvailable
+                        };
                         _context.Add(singleReservationChange);
+                        _publishEndpoint.Publish<ChangedReservationEvent>(singleReservationChangeEvent);
                     }
                         
                 }
@@ -75,14 +97,50 @@ namespace Reservation.Services
                             ChangeDate = reservationChange.ChangeDate,
                             ReservationAvailable = reservationChange.ReservationAvailable
                         };
+                        var singleReservationChangeEvent = new ChangedReservationEvent()
+                        {
+                            ReservationId = affectedReservation.ReservationId,
+                            HotelId = reservationChange.HotelId,
+                            HotelName = reservationChange.HotelName,
+                            BigRoomNumberChange = reservationChange.BigRoomNumberChange,
+                            SmallRoomNumberChange = reservationChange.SmallRoomNumberChange,
+                            BreakfastAvailable = reservationChange.BreakfastAvailable,
+                            WifiAvailable = reservationChange.WifiAvailable,
+                            ChangeInHotelPrice = reservationChange.ChangeInHotelPrice,
+                            HotelAvailable = reservationChange.HotelAvailable,
+                            TransportId = reservationChange.TransportId,
+                            ChangeInTransportPrice = reservationChange.ChangeInTransportPrice,
+                            FreeSeatsChange = reservationChange.FreeSeatsChange,
+                            PlaneAvailable = reservationChange.PlaneAvailable,
+                            ReservationAvailable = reservationChange.ReservationAvailable
+                        };
                         _context.Add(singleReservationChange);
+                        _publishEndpoint.Publish<ChangedReservationEvent>(singleReservationChangeEvent);
                     }
                 }
             }
             // reservation change applies to a single reservation
             else
             {
+                var singleReservationChangeEvent = new ChangedReservationEvent()
+                {
+                    ReservationId = reservationChange.ReservationId,
+                    HotelId = reservationChange.HotelId,
+                    HotelName = reservationChange.HotelName,
+                    BigRoomNumberChange = reservationChange.BigRoomNumberChange,
+                    SmallRoomNumberChange = reservationChange.SmallRoomNumberChange,
+                    BreakfastAvailable = reservationChange.BreakfastAvailable,
+                    WifiAvailable = reservationChange.WifiAvailable,
+                    ChangeInHotelPrice = reservationChange.ChangeInHotelPrice,
+                    HotelAvailable = reservationChange.HotelAvailable,
+                    TransportId = reservationChange.TransportId,
+                    ChangeInTransportPrice = reservationChange.ChangeInTransportPrice,
+                    FreeSeatsChange = reservationChange.FreeSeatsChange,
+                    PlaneAvailable = reservationChange.PlaneAvailable,
+                    ReservationAvailable = reservationChange.ReservationAvailable
+                };
                 _context.ReservationChanges.Add(reservationChange);
+                _publishEndpoint.Publish<ChangedReservationEvent>(singleReservationChangeEvent);
             }
             _context.SaveChanges();
         }
